a8d4ee5e331a11f81c024ca35ae61a3ee4aa2a90
zafri7410 <zafriazman@gmail.com>
2024-04-16 11:30:41 +0800

    Edit flops calculation to use output height and width instead of using input

--- RL/flops_calculation.py
+++ RL/flops_calculation.py
@@ -10,7 +10,7 @@ from networks.cnn1d_NiN import NiN_CNN1D_TrafficClassification
 
 def calc_perlayer_flops(orig_model, preserve_ratios):
 
-    # Formula is (2 * {c_in * [c_out * preserve_ratio] * k_height * k_width} + [c_out * preserve_ratio]) * h_in_height * h_in_width
+    # Formula is (2 * {c_in * [c_out * preserve_ratio] * k_height * k_width} + [c_out * preserve_ratio]) * h_out_height * h_out_width
     # the 2 * {} is because 1 MAC is 2 FLOPs (1 multiply & 1 addition)
     # c_in (input channel) is equal to each kernel's number of channel (eg. 224x224x3 image can only mul with _x_x3 kernel to produce 1 out channel)
     # c_out is equal to the number of kernel (eg. if input is 224x224x3, and we want to get 224x224x64 output, we need 64 of _x_x3 kernel)
@@ -19,15 +19,15 @@ def calc_perlayer_flops(orig_model, preserve_ratios):
     # 1500 is the number of input's features, which is the maximum transmission unit
 
     model = copy.deepcopy(orig_model)
-    input_perlayers = None
+    output_perlayers = None
     flops_list = []
     i = 0
 
     if model.__class__.__name__ == "CNN1D_TrafficClassification":
-        input_perlayers = [1500, 500, 166, 83, 41]  # either use torchsummary or manually calc based on pooling ops
+        output_perlayers = [1500, 500, 166, 83, 41]  # either use torchsummary or manually calc based on pooling ops
 
     elif model.__class__.__name__ == "NiN_CNN1D_TrafficClassification":
-        input_perlayers = [750, 750, 750, 375, 375, 375, 187, 187, 187, 187]
+        output_perlayers = [750, 750, 750, 375, 375, 375, 187, 187, 187, 187]
 
     else:
         raise NotImplementedError(f"Did not implement calc_perlayer_flops for {model.__class__.__name__}")
@@ -41,10 +41,10 @@ def calc_perlayer_flops(orig_model, preserve_ratios):
             c_out = module.out_channels
             k_height = 1
             k_width = module.kernel_size[0]
-            h_in_height  = 1
-            h_in_width = input_perlayers[i]
+            h_out_height  = 1
+            h_out_width = output_perlayers[i]
 
-            layer_flops = (2 * (c_in * (c_out * preserve_ratios[i]) * k_height * k_width) + (c_out * preserve_ratios[i])) * h_in_height * h_in_width
+            layer_flops = (2 * (c_in * (c_out * preserve_ratios[i]) * k_height * k_width) + (c_out * preserve_ratios[i])) * h_out_height * h_out_width
             flops_list.append(layer_flops)
 
             i += 1
